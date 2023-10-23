dc2ea59b7b1043c749b69dba40f39aee085159f3
EstebanEr-03 <125702762+EstebanEr-03@users.noreply.github.com>
2023-10-22 20:50:40 -0500

    Add files via upload

---
+++ Data/AplicationDBContext.cs
@@ -0,0 +1,42 @@
+﻿using APIProductos.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace APIProductos.Data
+{
+    public class AplicationDBContext :DbContext
+    {
+        public AplicationDBContext(
+            DbContextOptions<AplicationDBContext> options) : base(options)
+
+        {
+            
+        }
+
+        public DbSet<Producto> Producto { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Producto>().HasData(
+
+                new Producto
+                {
+                    IdProducto = 1,
+                    Nombre="Producto1",
+                    Descripcion="Producto1",
+                    cantidad=2
+                },
+                new Producto
+                {
+                    IdProducto = 2,
+                    Nombre="Producto2",
+                    Descripcion="Producto2",
+                    cantidad=324
+                }
+
+
+                );
+        }
+    }
+}
---
+++ Migrations/20231017232211_IngresarProducto2.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIProductos.Migrations
+{
+    /// <inheritdoc />
+    public partial class IngresarProducto2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Producto",
+                columns: new[] { "IdProducto", "Descripcion", "Nombre", "cantidad" },
+                values: new object[] { 2, "Producto2", "Producto2", 324 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Producto",
+                keyColumn: "IdProducto",
+                keyValue: 2);
+        }
+    }
+}
